00400c367efe61305e9c906e392194115eac5df0
Samuel Adams <sama29571@gmail.com>
2024-11-14 19:25:20 +0000

    seed db

---
+++ seeds.js
@@ -0,0 +1,38 @@
+//seed the db so you have some data to start out with
+const Product=require('./models/product')
+const mongoose=require('mongoose')
+const print=console.log
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/shopApp');
+  }
+  main().then(()=>{
+      print('connection sauce')
+  }).
+  catch(err => console.log(err));
+// const product=new Product({
+//     name:'grape',
+//     price:5,
+//     category:'fruit'
+// })
+// product.save().then(
+//     (res)=>{
+//         print('yeah it worked')
+//         print(res)
+//     }
+// ).catch((e)=>{
+//     print(e)
+// })
+
+Product.insertMany([
+    {
+        name:'passion fruit',
+        price:15,
+        category:'fruit'
+    },
+    {
+        name:'carrot',
+        price:3,
+        category:'vegetable'
+    },
+
+])
