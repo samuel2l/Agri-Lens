0f014b37d91869c21b01f3236112a700a0d26c41
Samuel Adams <sama29571@gmail.com>
2024-11-14 23:41:51 +0000

    add delete product

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app=express()
 const mongoose=require('mongoose')
 const Product=require('./models/product')
 const print=console.log
+const methodOverride=require('method-override')//to allow us use patch put type requests
+app.use(methodOverride('_method'))
+
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 app.use(express.urlencoded({extended:true}))
@@ -31,9 +34,14 @@ app.post('/products',async (req,res)=>{
     const {name,price,category}=req.body
     print('results from post request!!!!!!!!!!!')
     const newProduct= new Product({name,price,category})
-    newProduct.save().then((res)=>{print(res)}).catch((e)=>{print(e)})
+    newProduct.save()
+    .then(
+        (res)=>{
+            print(res)
+        })
+    .catch((e)=>{print(e)})
     
-    res.send('be like it sauce ooooo')
+    res.redirect('/products')
 
 })
 
@@ -49,6 +57,39 @@ app.get('/products/:id',async (req,res)=>{
     res.render('product_details.ejs',{product:product[0]})
 
 })
+app.get('/products/:id/edit',async(req,res)=>{
+    const id=req.params.id
+    const product=await Product.find({_id:id})
+
+    res.render('update_product.ejs',{product:product[0],id})
+
+})
+
+
+app.patch('/products/:id',async (req,res)=>{
+    const id=req.params.id
+    
+    let product=await Product.findByIdAndUpdate(id,req.body,{runValidators:true,new:true})
+    //new true means it will return the updated instance.
+
+    print(product)
+    print('HMMMMM')
+    print(req.body)
+    res.redirect(`/products/${id}`)
+
+})
+
+
+app.delete('/products/:id',async (req,res)=>{
+    const id=req.params.id
+    
+    let deletedProduct=await Product.findByIdAndDelete(id)
+    //new true means it will return the updated instance.
+
+    res.redirect(`/products`)
+
+})
+
 
 
 
