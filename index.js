92bbda0b2b56d97363e5531022e1d8f4ac9bf6fe
Samuel Adams <sama29571@gmail.com>
2024-11-14 18:54:46 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const path=require('path')
+const app=express()
+const mongoose=require('mongoose')
+const print=console.log
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'view'))
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/shopApp');
+  }
+  main().then(()=>{
+      print('connection sauce')
+  }).
+  catch(err => console.log(err));
+  
+
+app.listen(3000,()=>{
+    print('server up and running')
+    
+})
