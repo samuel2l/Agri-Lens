846fd995a89ab1db94819db58603403764850e18
Samuel Adams <sama29571@gmail.com>
2024-11-14 22:15:04 +0000

    add create product

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Product=require('./models/product')
 const print=console.log
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
-
+app.use(express.urlencoded({extended:true}))
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/shopApp');
   }
@@ -14,7 +14,11 @@ async function main() {
       print('connection sauce')
   }).
   catch(err => console.log(err));
-  
+
+  app.get('/products/new', (req,res)=>{
+ res.render('add_product.ejs')
+})
+
 
 app.get('/products',async (req,res)=>{
     const products=await Product.find({})
@@ -23,6 +27,16 @@ app.get('/products',async (req,res)=>{
     res.render('products.ejs',{products})
 
 })
+app.post('/products',async (req,res)=>{
+    const {name,price,category}=req.body
+    print('results from post request!!!!!!!!!!!')
+    const newProduct= new Product({name,price,category})
+    newProduct.save().then((res)=>{print(res)}).catch((e)=>{print(e)})
+    
+    res.send('be like it sauce ooooo')
+
+})
+
 
 app.get('/products/:id',async (req,res)=>{
     const id=req.params.id
@@ -35,6 +49,10 @@ app.get('/products/:id',async (req,res)=>{
     res.render('product_details.ejs',{product:product[0]})
 
 })
+
+
+
+
 
 app.listen(3000,()=>{
     print('server up and running')
